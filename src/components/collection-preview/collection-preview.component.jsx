2f07acd227ca626d37874438a91c5575e11cebc6
Anuar Abdullahi <anuar.abdullahi@kinetit.com>
2020-01-21 17:51:15 +0000

    updated shop data with new data, updated cart utils to group times in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,9 +6,12 @@ const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item, idx) => idx < 4).map(({ id, ...itemProps }) => (
-                <CollectonItem key={id} {...itemProps} />
-            ))}
+            {items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
+                    <CollectonItem key={item.id} item={item} />
+                ))
+            }
         </div>
     </div>
 );
