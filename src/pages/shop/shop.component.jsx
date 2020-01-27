ba13283b5129c659720aec1d0fb2760b7497d7bd
Anuar Abdullahi <anuar.abdullahi@kinetit.com>
2020-01-27 23:58:21 +0000

    Moved our directory and shop date into its own reducer, created respective selectors and updated corresponding components with new redux flfow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props)
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-        this.state = {
-            collection: SHOP_DATA
-        }
-    }
 
-    render() {
-        const { collection } = this.state;
-
-        return (
-            <div className='shop-page'>
-                {
-                    collection.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route
+            exact
+            path={`${match.path}`}
+            component={CollectionOverview}
+        />
+        <Route
+            path={`${match.path}/:collectionId`}
+            component={CollectionPage}
+        />
+    </div>
+);
 
 export default ShopPage;
