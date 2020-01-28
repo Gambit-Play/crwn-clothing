9e5a8bfd367abc8cd31cf2e67d388dbd1ff68edf
Anuar Abdullahi <anuar.abdullahi@kinetit.com>
2020-01-28 01:24:15 +0000

    Updating Stripe integration and updating our favicon and tile

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -31,6 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         )}
         <div className="total"><span>TOTAL: ${total}</span></div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
